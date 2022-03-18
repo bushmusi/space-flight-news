f811f2d346b69eb77e0bf95b312c3b28c7b0e66f
AckonSamuel <ackons433@gmail.com>
2022-03-18 11:44:29 +0000

    Api set up for fetching comments

---
+++ src/assets/js/logic/addComment.js
@@ -0,0 +1,24 @@
+const BASE_URL = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/';
+const appId = 'spacenew01';
+
+const getCommentsData = async (newsId) => {
+  let result = await fetch(`${BASE_URL}${appId}/comments?item_id=${newsId}`);
+  result = await result.json();
+  return result;
+};
+
+const addComment = async (newsId, title, comments) => {
+  await fetch(`${BASE_URL}${appId}/comments`, {
+    method: 'POST',
+    body: JSON.stringify({
+      item_id: newsId,
+      username: title,
+      comment: comments,
+    }),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+};
+
+export { getCommentsData, addComment };
