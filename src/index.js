513b3619c87b3fa436d1f18a0d6ab2dd71735036
bushmusi <bushera.mestofa@gmail.com>
2022-03-19 14:56:36 +0300

    Add like logic

--- src/index.js
+++ src/index.js
@@ -5,23 +5,37 @@ import './assets/scss/main-tag.scss';
 import News from './assets/js/logic/news-fetch.js';
 import display from './assets/js/dom-service/display-news.js';
 import modal from './assets/js/dom-service/display-comment.js';
-import { initializeComments } from './assets/js/logic/initializeComment.js';
+import likeCounter from './assets/js/logic/like-counter';
 
 const newsService = new News();
-const data = newsService.getArticles();
-let getIndex;
+const likeService = new likeCounter();
 
 const getAllNews = async () => {
-  const data = await newsService.getArticles();
-  display(data);
+  const newsData = await newsService.getArticles();
+  const likeData = await likeService.get();
+  display(newsData,likeData);
+
+  const likeElements = document.querySelectorAll('.like-icon');
+  likeElements.forEach( (val) => {
+    val.addEventListener('click',async (e) => {
+      let id = val.id.split('-');
+      const like_id = +id[2];
+      likeService.like(like_id);
+      const likeItem = document.getElementById(val.id);
+      let counter = e.target.innerText;
+      counter = +counter + 1;
+      likeItem.innerHTML = counter;
+    })
+  })
+
 };
 
 getAllNews();
 
+
 const getComment = async (g) => {
   const result = await fetch(`https://api.spaceflightnewsapi.net/v3/articles/${g}`).then((response) => response.json())
     .catch((err) => new Error(err));
-  // console.log(result);
   return result;
 };
 
@@ -29,7 +43,7 @@ document.addEventListener('click', async (e) => {
   e.preventDefault();
   let targetd = e.target.classList.contains('comment-span');
   if (targetd) {
-    getIndex = e.target.getAttribute('id');
+    const getIndex = e.target.getAttribute('id');
 
     const ag = Number(getIndex);
     const gg = await getComment(ag);
@@ -37,9 +51,6 @@ document.addEventListener('click', async (e) => {
     document.querySelector('main').classList.toggle('dn');
   }
   targetd = !e.target.classList.contains('comment-span');
-  // const letit = await initializeComments();
-  // const gup = Number(letit);
-  // console.log(gup);
 });
 
 document.addEventListener('click', async (e) => {
