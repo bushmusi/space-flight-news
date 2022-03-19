167d6a73be4941a2ae5fd44ba8567d9545751fcd
bushmusi <bushera.mestofa@gmail.com>
2022-03-19 14:58:17 +0300

    change name of comment

---
+++ src/assets/js/logic/add-comment.js
@@ -0,0 +1,23 @@
+import { BASE_URL, appId } from "../common-var";
+
+const getCommentsData = async (newsId) => {
+  let result = await fetch(`${BASE_URL}${appId}/comments?item_id=${newsId}`);
+  result = await result.json();
+  return result;
+};
+
+const addComment = async (newsId, title, comments) => {
+  await fetch(`${BASE_URL}${appId}/comments`, {
+    method: 'POST',
+    body: JSON.stringify({
+      item_id: newsId,
+      username: title,
+      comment: comments,
+    }),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+};
+
+export { getCommentsData, addComment };
