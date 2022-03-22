f0cb7ddb3b18ad0758c1dd5e5d7b36b256098414
bushmusi <bushera.mestofa@gmail.com>
2022-03-22 11:16:44 +0300

    Refactor the code

--- src/assets/js/dom-service/display-news.js
+++ src/assets/js/dom-service/display-news.js
@@ -1,71 +1,10 @@
-const MAIN_CONT = document.querySelector('main');
+import loadNews from './news-component/load-news.js';
 
-const getItemBox = () => {
-  const itemBox = document.createElement('div');
-  itemBox.className = 'item-box';
-  return itemBox;
-};
-
-const getItemChild = () => {
-  const img = document.createElement('img');
-
-  const titleElement = document.createElement('strong');
-
-  const descElement = document.createElement('p');
-
-  const interactionCont = document.createElement('div');
-  interactionCont.className = 'interact-icon';
-
-  const reserveBtn = document.createElement('button');
-  reserveBtn.className = 'reserve-btn';
-  reserveBtn.textContent = 'Reservation';
-
-  return [img, titleElement, descElement, interactionCont, reserveBtn];
-};
-
-const getIcons = () => {
-  const commentSpan = document.createElement('span');
-  commentSpan.className = 'fa fa-comment comment-span';
-  const likeSpan = document.createElement('span');
-  likeSpan.className = 'fa fa-heart like-icon';
-  return [commentSpan, likeSpan];
-};
 
 const displayItem = async (newsData, likesData) => {
-  MAIN_CONT.innerHTML = '';
 
-  newsData.forEach((item,index) => {
-    const [img, titleElement, descElement, interactionCont, reserveBtn] = getItemChild();
-    const itemBox = getItemBox();
-    img.src = item.imageUrl;
-    const imgBox = document.createElement('div');
-    imgBox.className = 'item-image'
-    imgBox.appendChild(img);
-    titleElement.textContent = item.title;
-    const descBox = document.createElement('div');
-    descBox.className = 'desc-box'
-    descElement.textContent = item.summary;
-    descBox.appendChild(descElement);
-    const [commentSpan, likeSpan] = getIcons();
-    commentSpan.id = item.id;
-    likeSpan.id = `like-icon-${item.id}`;
-    [commentSpan, likeSpan].forEach((element) => {
-      interactionCont.appendChild(element);
-    });
+  loadNews(newsData,likesData);
 
-    const itemElements = [imgBox, titleElement, descBox, interactionCont, reserveBtn];
-    itemElements.forEach((val) => {
-      itemBox.appendChild(val);
-    });
-    MAIN_CONT.appendChild(itemBox);
-  });
-
-  if (likesData.length > 0) {
-    likesData.forEach((val) => {
-      const item = document.getElementById(`like-icon-${val.item_id}`);
-      const temp = item ? item.innerHTML = ` ${val.likes}` : '';
-    });
-  }
 };
 
 export default displayItem;
